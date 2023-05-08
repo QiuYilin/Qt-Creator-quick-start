5e1f253d94851d44013c75a121d4e43750b39062
qiuyilin <qiu.yilin@qq.com>
2023-05-08 17:06:27 +0800

    16

--- build/16/16-1/modelview1/CMakeFiles/modelview1.dir/modelview1_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-16/16-1/modelview1/CMakeFiles/modelview1.dir/modelview1_autogen/mocs_compilation.cpp.o: \
- /home/qiuyilin/Program/book_code/Qt-Creator-quick-start/build/16/16-1/modelview1/modelview1_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/16/16-2/modelview1/CMakeFiles/modelview2_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/modelview2_autogen"
-  "modelview2_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/modelview2_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
